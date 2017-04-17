c9051d371c10aef0d57e52a147ad70cc2d866972
Kathleen Catlett <kathleencatlett@comcast.net>
2017-04-17 13:46:33 -0400

    fixed mongo URI???

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.set("view engine", "handlebars");
 
 
 //server
-mongoose.connect(mongolab-contoured-49399);
+mongoose.connect('mongodb://heroku_zxc81h22:33c4ht2ilg9gcm1b3c8h4rpt9tc@ds161640.mlab.com:61640/heroku_zxc81h22');
 
 var db = mongoose.connection;
 
